e2e8bdd145176f03d1787d9e44687e453c01d4fd
Jhon Jairo Cordoba Ordoñez <cordobaojhonjairo21@gmail.com>
2025-08-06 19:34:43 -0500

    Update contentHTML.js

--- src/modules/contentHTML.js
+++ src/modules/contentHTML.js
@@ -5,7 +5,9 @@ export default function contentHTML(element, issue) {
         let codeTem = `
             <article id=${item.id} class="module">
                 <h2>${item.title}</h2>
-                <h4>${item.description}</h4>
+                ${item.description ? `
+                    <h4>${item.description}</h4>
+                ` : ""}
                 <div>${item.content}</div>
                 ${item.example ? `
                     <pre class="code">
@@ -18,4 +20,5 @@ export default function contentHTML(element, issue) {
     }
 
     element.innerHTML = code;
+
 }
